b57dae7170d4140933e042898018bea01c42cfcc
Simeon Lee <simeonhlee@gmail.com>
2016-08-01 14:10:36 -0700

    Implement constructor and forage method to ForagerBee.js using ES6

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,15 @@
 import Bee from './Bee'
 
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
 
 export default ForagerBee;
