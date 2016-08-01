873dcc838f85e04a135afc7c09768b06c40bc216
Simeon Lee <simeonhlee@gmail.com>
2016-08-01 14:17:08 -0700

    Implement constructor and methods to RetiredForagerBee.js using ES6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,18 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+  	this.age = 40;
+  	this.job = 'gamble';
+  	this.canFly = false;
+    this.color = 'grey';
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
 
 export default RetiredForagerBee;
