ce212b4ac2eaa8fbdf86677cdaf9481a551eaec4
Simeon Lee <simeonhlee@gmail.com>
2016-08-01 14:05:25 -0700

    Syntax refinement

--- src/Grub.js
+++ src/Grub.js
@@ -1,11 +1,11 @@
 class Grub {
 	constructor () {
-    this.age = 0
-    this.color = 'pink'
-    this.food = 'jelly'
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
 	}
   eat () {
-    return 'Mmmmmmmmm jelly'
+    return 'Mmmmmmmmm jelly';
   }
 };
 
