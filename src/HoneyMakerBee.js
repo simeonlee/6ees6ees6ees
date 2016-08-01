41b56779e451d2a58107f0d8db8b59755c67f6db
Simeon Lee <simeonhlee@gmail.com>
2016-08-01 14:13:32 -0700

    Implement constructor and methods to HoneyMakerBee.js using ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,19 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+  makeHoney() {
+  	this.honeyPot++;
+  }
+  giveHoney() {
+  	if ( this.honeyPot - 1 >= 0 ) {
+  		this.honeyPot--;
+  	}
+  }
 };
 
 export default HoneyMakerBee;
